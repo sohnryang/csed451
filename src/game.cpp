28dd381c1ffedfea5fcb36286c670ef34f197611
김경민(IT융합공학과) <m4080m@postech.ac.kr>
2024-03-08 19:40:29 +0900

    feat(game): add functions for drawing background and tree

--- src/game.cpp
+++ src/game.cpp
@@ -21,43 +21,61 @@ void display() { ctx_ptr->update(); }
 
 void idle() { glutPostRedisplay(); }
 
+// TODO: map generation
+// divide the map with NxN grid. let every entity placement, movement and collision check can be done by those grid
+// row 0 (start) and row N-1 (end) is fixed to be a ground, the other are randomly chosen to be a ground or a road
+// on the ground row, place woods at randomly chosen 0 ~ N/2 columns
+// on the road row, randomly choose the direction and speed of the cars
+// draw dotted line between continuous road rows
+
+const size_t grid_size = 8;
+const double step_size = 2.0f / grid_size;
+
+void fill_map_row(size_t row_index, const components::Color& color) {
+    ctx_ptr->registry().add_render_info(
+        *ctx_ptr, {{glm::vec4(-1.0, -1.0 + step_size * row_index, 0.0, 1.0),
+                    glm::vec4(1.0, -1.0 + step_size * row_index, 0.0, 1.0),
+                    glm::vec4(1.0, -1.0 + step_size * (row_index + 1), 0.0, 1.0),
+                    glm::vec4(-1.0,-1.0 + step_size * (row_index + 1), 0.0, 1.0)},
+                   color });
+}
+
+void create_tree(size_t row_index, size_t col_index, const components::Color& color) {
+    // might move this constant (0.75) to somewhere
+    const double tree_size = step_size * 0.75;
+    const double actual_pos_x = -1.0 + step_size * col_index;
+    const double actual_pos_y = -1.0 + step_size * row_index;
+    const double gap = (step_size - tree_size) / 2.0;
+    ctx_ptr->registry().add_render_info(
+        *ctx_ptr, { {glm::vec4(actual_pos_x + gap, actual_pos_y + gap, 1.0, 1.0),
+                    glm::vec4(actual_pos_x + gap + tree_size, actual_pos_y + gap, 1.0, 1.0),
+                    glm::vec4(actual_pos_x + gap + tree_size, actual_pos_y + gap + tree_size, 1.0, 1.0),
+                    glm::vec4(actual_pos_x + gap, actual_pos_y + gap + tree_size, 1.0, 1.0)},
+                   color });
+
+    // TODO: add tree context
+}
+
 void create_map() {
   const components::Color grass_color = {68.0 / 255, 132.0 / 255, 46.0 / 255},
-                          road_color = {172.0 / 255, 172.0 / 255, 172.0 / 255};
+                          road_color = {172.0 / 255, 172.0 / 255, 172.0 / 255},
+                          tree_color = {200.0 / 255, 131.0 / 255, 0.0 / 255};
 
-  const float step_size = 2.0f / 8;
-  ctx_ptr->registry().add_render_info(
-      *ctx_ptr, {{glm::vec4(-step_size * 4, -step_size * 4, 0.0, 1.0),
-                  glm::vec4(step_size * 4, -step_size * 4, 0.0, 1.0),
-                  glm::vec4(step_size * 4, -step_size * 3, 0.0, 1.0),
-                  glm::vec4(-step_size * 4, -step_size * 3, 0.0, 1.0)},
-                 grass_color});
-  ctx_ptr->registry().add_render_info(
-      *ctx_ptr, {{
-                     glm::vec4(-step_size * 4, -step_size * 3, 0.0, 1.0),
-                     glm::vec4(step_size * 4, -step_size * 3, 0.0, 1.0),
-                     glm::vec4(step_size * 4, -step_size * 1, 0.0, 1.0),
-                     glm::vec4(-step_size * 4, -step_size * 1, 0.0, 1.0),
-                 },
-                 road_color});
-  ctx_ptr->registry().add_render_info(
-      *ctx_ptr, {{glm::vec4(-step_size * 4, -step_size * 1, 0.0, 1.0),
-                  glm::vec4(step_size * 4, -step_size * 1, 0.0, 1.0),
-                  glm::vec4(step_size * 4, step_size * 0, 0.0, 1.0),
-                  glm::vec4(-step_size * 4, step_size * 0, 0.0, 1.0)},
-                 grass_color});
-  ctx_ptr->registry().add_render_info(
-      *ctx_ptr, {{glm::vec4(-step_size * 4, step_size * 0, 0.0, 1.0),
-                  glm::vec4(step_size * 4, step_size * 0, 0.0, 1.0),
-                  glm::vec4(step_size * 4, step_size * 3, 0.0, 1.0),
-                  glm::vec4(-step_size * 4, step_size * 3, 0.0, 1.0)},
-                 road_color});
-  ctx_ptr->registry().add_render_info(
-      *ctx_ptr, {{glm::vec4(-step_size * 4, step_size * 3, 0.0, 1.0),
-                  glm::vec4(step_size * 4, step_size * 3, 0.0, 1.0),
-                  glm::vec4(step_size * 4, step_size * 4, 0.0, 1.0),
-                  glm::vec4(-step_size * 4, step_size * 4, 0.0, 1.0)},
-                 grass_color});
+  fill_map_row(0, grass_color);
+  fill_map_row(1, road_color);
+  fill_map_row(2, road_color);
+  fill_map_row(3, grass_color);
+  fill_map_row(4, road_color);
+  fill_map_row(5, road_color);
+  fill_map_row(6, road_color);
+  fill_map_row(7, grass_color);
+
+  create_tree(3, 0, tree_color);
+  create_tree(3, 3, tree_color);
+  create_tree(3, 6, tree_color);
+  create_tree(7, 2, tree_color);
+  create_tree(7, 3, tree_color);
+  create_tree(7, 5, tree_color);
 }
 
 int main(int argc, char **argv) {
