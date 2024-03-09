1fdafc6dc37b325165acc7dd1c00660b3cf79b37
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-09 13:29:07 +0900

    feat(game): add utility functions

---
+++ include/utils.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include <utility>
+
+#include "components.hpp"
+
+using BoundingBox = std::pair<glm::vec2, glm::vec2>;
+
+BoundingBox bounding_box(const components::RenderInfo &render_info,
+                         const components::Transform &transform);
+
+bool intersect(const BoundingBox &box1, const BoundingBox &box2);
---
+++ src/utils.cpp
@@ -0,0 +1,35 @@
+#include "utils.hpp"
+
+#include <glm/glm.hpp>
+
+#include <glm/ext/matrix_transform.hpp>
+
+#include <algorithm>
+#include <vector>
+
+#include "components.hpp"
+
+BoundingBox bounding_box(const components::RenderInfo &render_info,
+                         const components::Transform &transform) {
+  const glm::mat4 mat = glm::translate(glm::mat4(1), transform.disp);
+  std::vector<glm::vec4> transformed;
+  const auto &vertices = render_info.vertices;
+  std::transform(vertices.cbegin(), vertices.cend(),
+                 std::back_inserter(transformed),
+                 [&mat](const glm::vec4 &vertex) { return mat * vertex; });
+  float xmin = transformed[0][0], xmax = transformed[0][0],
+        ymin = transformed[0][1], ymax = transformed[0][1];
+  for (const auto &v : transformed) {
+    xmin = std::min(xmin, v[0]);
+    xmax = std::max(xmax, v[0]);
+    ymin = std::min(ymin, v[1]);
+    ymax = std::max(ymax, v[1]);
+  }
+
+  return {glm::vec2(xmin, ymax), glm::vec2(xmax, ymin)};
+}
+
+bool intersect(const BoundingBox &box1, const BoundingBox &box2) {
+  return box1.first[0] <= box2.second[0] && box2.first[0] <= box1.second[0] &&
+         box1.second[1] <= box2.first[1] && box2.second[1] <= box1.first[1];
+}
