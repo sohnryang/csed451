f152fec403cd3d532713e82275479981c3b28a76
Ryang Sohn <loop.infinitely@gmail.com>
2024-03-26 01:15:25 +0900

    feat(ecs): add entity graph

--- include/ecs/entities.hpp
+++ include/ecs/entities.hpp
@@ -2,16 +2,24 @@
 
 #include <cstddef>
 #include <queue>
+#include <unordered_map>
+#include <vector>
 
 namespace ecs {
 namespace entities {
 using EntityId = std::size_t;
+
+struct EntityGraphNode {
+  EntityId parent;
+  std::vector<EntityId> children;
+};
 
 class EntityManager {
 private:
   EntityId _end_id;
   const EntityId _max_entities;
   std::queue<EntityId> _vacant_ids;
+  std::unordered_map<EntityId, EntityGraphNode> _entity_graph;
 
 public:
   EntityManager();
@@ -22,6 +30,7 @@ public:
   EntityId next_id();
   EntityId end_id() const;
   void remove_id(EntityId id);
+  std::unordered_map<EntityId, EntityGraphNode> &entity_graph();
 };
 } // namespace entities
 } // namespace ecs
--- src/ecs/entities.cpp
+++ src/ecs/entities.cpp
@@ -1,24 +1,46 @@
 #include "ecs/entities.hpp"
+
+#include <algorithm>
 #include <stdexcept>
+#include <unordered_map>
 
 using namespace ecs::entities;
 
 EntityManager::EntityManager() : EntityManager(4096) {}
 
 EntityManager::EntityManager(EntityId max_entities)
-    : _end_id(0), _max_entities(max_entities), _vacant_ids() {}
+    : _end_id(0), _max_entities(max_entities), _vacant_ids(), _entity_graph() {}
 
 EntityId EntityManager::next_id() {
+  EntityId new_id;
   if (!_vacant_ids.empty()) {
     const auto id = _vacant_ids.front();
     _vacant_ids.pop();
-    return id;
-  }
-  if (_end_id >= _max_entities)
+    new_id = id;
+  } else if (_end_id >= _max_entities)
     throw std::length_error("Out of available entities");
-  return _end_id++;
+  else
+    new_id = _end_id++;
+
+  _entity_graph[new_id] = {new_id, {}};
+  return new_id;
 }
 
 EntityId EntityManager::end_id() const { return _end_id; }
 
-void EntityManager::remove_id(EntityId id) { _vacant_ids.push(id); }
+void EntityManager::remove_id(EntityId id) {
+  if (!_entity_graph.count(id))
+    throw std::out_of_range("ID not found");
+
+  const auto &node = _entity_graph[id];
+  auto &parent_children = _entity_graph[node.parent].children;
+  const auto it =
+      std::find(parent_children.cbegin(), parent_children.cend(), id);
+  parent_children.erase(it);
+  _entity_graph.erase(id);
+  _vacant_ids.push(id);
+}
+
+std::unordered_map<EntityId, EntityGraphNode> &EntityManager::entity_graph() {
+  return _entity_graph;
+}
